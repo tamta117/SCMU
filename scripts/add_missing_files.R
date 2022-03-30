91827f9106162cc489212d6893b6226047187e6d
tamta117 <tamtar1@uw.edu>
2022-03-30 16:02:35 -0700

    made table of all processed wav files

--- scripts/add_missing_files.R
+++ scripts/add_missing_files.R
@@ -4,19 +4,20 @@ library(dplyr)
 library(data.table)
 library(tidyverse)
 library(lubridate)
+library(stringi)
 
 #import cam csv
-lava1_cam<-read.csv(here("odata/camera/lava1_cam.csv"))
-lava2_cam<-read.csv(here("odata/camera/lava2_cam.csv"))
-moss_cam<-read.csv(here("odata/camera/moss_cam.csv"))
-pinnacle_cam<-read.csv(here("odata/camera/pinnacle_cam.csv"))%>%
+lava1_cam<-read.csv(here("data/camera/lava1_cam.csv"))
+lava2_cam<-read.csv(here("data/camera/lava2_cam.csv"))
+moss_cam<-read.csv(here("data/camera/moss_cam.csv"))
+pinnacle_cam<-read.csv(here("data/camera/pinnacle_cam.csv"))%>%
   mutate(Murrelet=ifelse(Murrelet=="true",TRUE,FALSE))
-refuge_cam<-read.csv(here("odata/camera/refuge_cam.csv"))%>%
+refuge_cam<-read.csv(here("data/camera/refuge_cam.csv"))%>%
   mutate(Murrelet=ifelse(Murrelet=="true",TRUE,FALSE))
 
 #join all cam files
-all_cam<-rbindlist(list(lava1_cam,lava2_cam,moss_cam,
-                        pinnacle_cam,refuge_cam))
+all_cam<-rbindlist(list(lava1_cam, lava2_cam, moss_cam,
+                        pinnacle_cam, refuge_cam))
 #filter for ravens
 raven_cam<-all_cam%>%
   subset(all_cam$Species=="Raven")
@@ -31,8 +32,7 @@ raven_cam<-raven_cam%>%
   select(-time2)
 raven_cam$min<-str_pad(raven_cam$min, width=4, side="right", pad="0")
 raven_cam<-raven_cam%>%
-  unite("time_mp3",time1:min,remove=FALSE)
-raven_cam$time_mp3<-gsub("_","",as.character(raven_cam$time_mp3))
+  unite("time_mp3",time1:min,remove=FALSE, sep="")
 
 #change date format
 raven_cam$date_mp3<-parse_date_time(raven_cam$Date, orders = c('ymd', 'mdy'),tz="")
@@ -41,16 +41,34 @@ raven_cam$date_mp3<-gsub("-","",as.character(raven_cam$date_mp3))
 #merge time and date
 raven_cam<-raven_cam%>%
   select(-time1,-min,-min1)%>%
-  unite("datetime_mp3",date_mp3:time_mp3,remove=FALSE)
+  unite("datetime_mp3",date_mp3:time_mp3,remove=FALSE,sep="")
 
 #filter for each site
 lava1_det<-raven_cam%>%
   subset(Site=="Lava1")
 
-#make it look like wav file format
-lava1_det$datetime <- sub("^", "S4A04765_", lava1_det$datetime_mp3 )
-lava1_det$datetime <- paste0(lava1_det$datetime, ".wav")
-
 #create table of all wav
-df<-unique(lava1_det$datetime)
+df<-unique(lava1_det$datetime_mp3)
 lava1_det_mp3<-as.data.table(df,TRUE)
+lava1_det_num<-as.numeric(lava1_det_mp3$df)
+lava1_det_mp3<-as.data.table(lava1_det_num,TRUE)
+lava1_det_plus<-lava1_det_mp3[,1]+1000
+lava1_det_minus<-lava1_det_mp3[,1]-1000
+lava1_det_mp3<-as.character(lava1_det_mp3$lava1_det_num)
+lava1_det_mp3<-as.data.table(lava1_det_mp3,TRUE)
+lava1_det_plus<-as.character(lava1_det_plus$lava1_det_num)
+lava1_det_plus<-as.data.table(lava1_det_plus,TRUE)
+lava1_det_minus<-as.character(lava1_det_minus$lava1_det_num)
+lava1_det_minus<-as.data.table(lava1_det_minus,TRUE)
+colnames(lava1_det_plus)<-'lava1_det_mp3'
+colnames(lava1_det_minus)<-'lava1_det_mp3'
+lava1_det_all<-rbindlist(list(lava1_det_mp3,lava1_det_plus,
+                              lava1_det_minus))
+lava1_det_all<-unique(lava1_det_all$lava1_det_mp3)
+lava1_det_all<-as.data.table(lava1_det_all,TRUE)
+colnames(lava1_det_all)<-'lava1'
+
+#make it look like wav file format
+lava1_det_all$lava1 <- sub("^", "S4A04765_", lava1_det_all$lava1)
+lava1_det_all$lava1 <- paste0(lava1_det_all$lava1, ".wav")
+stri_sub(lava1_det_all$lava1, 18, 17) <- "_"
