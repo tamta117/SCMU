1910a829397e5bd08e54a7f0774161fa07be9d2c
Amelia <ameliaduvall@gmail.com>
2022-04-14 19:50:10 -0700

    added camera id, fixed detections for image type

--- scripts/camera_dat_process.R
+++ scripts/camera_dat_process.R
@@ -2,9 +2,6 @@
 ## 5 April 2022
 
 ## TODO
-# CORA and SCMU only
-# dataframe with all images (not just image set)
-# fix loop for detect_type difference
 
 ## load libraries
 library(tidyverse)
@@ -18,8 +15,9 @@ lava1 <-
              pattern = "*.csv", 
              full.names = T) %>% 
   map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
-  #dplyr::select(-c("...27")) %>%
-  mutate(Site = "Lava1") %>%
+  dplyr::select(-c("...27")) %>%
+  mutate(Site = "Lava1",
+         cam_id = c("cam_01")) %>%
   dplyr::select(Site, everything())
 
 lava2 <-
@@ -27,8 +25,9 @@ lava2 <-
              pattern = "*.csv", 
              full.names = T) %>% 
   map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
-  #dplyr::select(-c("...27")) %>%
-  mutate(Site = "Lava2") %>%
+  dplyr::select(-c("...27")) %>%
+  mutate(Site = "Lava2",
+         cam_id = c("cam_02")) %>%
   dplyr::select(Site, everything())
 
 moss <-
@@ -36,8 +35,9 @@ moss <-
              pattern = "*.csv", 
              full.names = T) %>% 
   map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
-  #dplyr::select(-c("...27")) %>%
-  mutate(Site = "Moss") %>%
+  dplyr::select(-c("...27")) %>%
+  mutate(Site = "Moss",
+         cam_id = ifelse(Folder == "Moss Camera 1", "cam_03", "cam_04")) %>%
   dplyr::select(Site, everything())
 
 pinnacle <-
@@ -45,8 +45,9 @@ pinnacle <-
              pattern = "*.csv", 
              full.names = T) %>% 
   map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
-  #dplyr::select(-c("...27")) %>%
-  mutate(Site = "Pinnacle") %>%
+  dplyr::select(-c("...27")) %>%
+  mutate(Site = "Pinnacle",
+         cam_id = ifelse(Folder == "Pinnacle Camera 1", "cam_05", "cam_06")) %>%
   dplyr::select(Site, everything())
 
 refuge <-
@@ -54,8 +55,9 @@ refuge <-
              pattern = "*.csv", 
              full.names = T) %>% 
   map_df(~read_csv(., col_types = cols(.default = "c"))) %>%
-  #dplyr::select(-c("...27")) %>%
-  mutate(Site = "Refuge") %>%
+  dplyr::select(-c("...27")) %>%
+  mutate(Site = "Refuge",
+         cam_id = c("cam_07")) %>%
   dplyr::select(Site, everything())
 
 ## combine into one dataframe, one row per camera observation
@@ -66,7 +68,7 @@ camera_raw_dat <- rbind(lava1, lava2, moss, pinnacle, refuge) %>%
          Count = as.integer(Count))
 
 ## export csv with all camera observations (not cleaned)
-write.csv(camera_raw_dat, here("Data", "camera", "camera_raw_dat.csv"), row.names = FALSE)
+# write.csv(camera_raw_dat, here("Data", "camera", "camera_raw_dat.csv"), row.names = FALSE)
 nrow(camera_raw_dat) # total observations/attempts
 
 ## clean dataframe and reformat
@@ -82,7 +84,8 @@ camera_dat_full <- camera_raw_dat %>%
          hour = hour(date_time), # pull hour
          min = minute(date_time), # pull minute
          sec = second(date_time), # pull second
-         image_type = ifelse(sec == 00, "timelapse", "motion"), # categorize image as timelapse or motion-activated based on second hand
+         min2 = substring(min, 2),  
+         image_type = ifelse(min == 0 & sec == 00 | min2 == 0 & sec == 00, "timelapse", "motion"), # categorize image as timelapse or motion-activated based on second hand
          int_diff = date_time - lag(date_time), # find interval between images taken
          int_diff_secs = as.numeric(int_diff, units = 'mins'), # compute to seconds
          murrelet_status = ifelse(murrelet == "true" & is.na(comments), "alive", # fix dead murrelets
@@ -102,30 +105,31 @@ camera_dat_full <- camera_raw_dat %>%
                                PEEM == 1 ~ 1,
                                predation == 1 ~ 1, TRUE ~ 0)) %>% 
   #mutate(burst = ifelse(int_diff_secs < 5, TRUE, FALSE)) %>% # filter out images if interval between them is <5 seconds (double-check this)
-  dplyr::select(folder, relative_path, file, image_no, site, date_time, date, time,
+  dplyr::select(folder, relative_path, file, image_no, site, cam_id, date_time, date, time,
                 year, month, day, jday, hour, min, sec, image_type, everything())
 
-write.csv(camera_dat_full, here("data", "camera", "camera_dat_full.csv"), row.names = FALSE)
+# write.csv(camera_dat_full, here("data", "camera", "camera_dat_full.csv"), row.names = FALSE)
 
-## create slimmed down dataframe with detections in 10 minute windows
-camera_dat_sml <- camera_dat_full %>%
-  dplyr::select(image_no, site, date, time, date_time, year, month, day, jday, hour, min, sec, image_type, detection,
-                SCMU, CORA, BNOW, PEEM, predation)
+## create dataframe for SCMU and CORA with all images 
+camera_dat_all_images <- camera_dat_full %>%
+  dplyr::select(image_no, site, cam_id, date, time, date_time, year, month, day, jday, hour, min, sec, image_type, detection,
+                SCMU, CORA)
 
-#write.csv(camera_dat, here("data", "camera", "camera_dat.csv"))
+# write.csv(camera_dat_all_images, here("data", "camera_dat_all_images.csv"), row.names = FALSE)
 
-## extract independent detections (set at 10 minutes, code from Sarah B. Bassing)
-dat <- camera_dat_sml %>%
-  dplyr::select(image_no, site, date_time, SCMU, CORA, BNOW, PEEM) %>%
-  pivot_longer(cols = c(SCMU, CORA, BNOW, PEEM), names_to = "species", values_to = "detection") %>%
+## extract independent detections (set at 10 minutes, code from Sarah B. Bassing) for image set
+dat <- camera_dat_all_images %>%
+  dplyr::select(image_no, site, date_time, image_type, SCMU, CORA) %>%
+  pivot_longer(cols = c(SCMU, CORA), names_to = "species", values_to = "detection") %>%
   filter(detection == 1) %>%
   arrange(site, date_time)
 caps <- c()
 caps[1] <- 1
 for (i in 2:nrow(dat)){
   if (dat$site[i-1] != dat$site[i]) caps[i] = i
+  if (dat$image_type[i-1] != dat$image_type[i]) caps[i] = i
   else (if (dat$species[i-1] != dat$species[i]) caps[i] = i
-        else (if (difftime(dat$date_time[i], dat$date_time[i-1], units = c("mins")) > 10) caps[i] = i
+        else (if (difftime(dat$date_time[i], dat$date_time[i-1], units = c("mins")) >= 10) caps[i] = i
               else caps[i] = caps[i-1]))
 }
 
@@ -144,10 +148,10 @@ detect <- capdata %>%
   pivot_wider(names_from = species, values_from = detection)
 
 ## create new dataframe with detection info
-camera_dat <- camera_dat_sml %>%
-  dplyr::select(-c(detection, SCMU, CORA, BNOW, PEEM, predation)) %>%
-  full_join(detect, by = c("image_no", "site", "date_time")) %>%
+camera_dat_image_set <- camera_dat_all_images %>%
+  dplyr::select(-c(detection, SCMU, CORA)) %>%
+  full_join(detect, by = c("image_no", "site", "date_time", "image_type")) %>%
   replace(is.na(.), 0) # replace NA values with 0
 
 ## export csv
-write.csv(camera_dat, here("data", "camera", "camera_dat.csv"))
+# write.csv(camera_dat_image_set, here("data", "camera_dat_image_set.csv"), row.names = FALSE)
